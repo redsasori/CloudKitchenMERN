afa9e559a6e8162ba4c6475f572ca2b182848a3c
Nilesh Mishra <91262795+redsasori@users.noreply.github.com>
2026-01-28 21:46:05 +0530

    Create Contact.js model for contacts

---
+++ server/models/Contact.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    trim: true
+  },
+  subject: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
